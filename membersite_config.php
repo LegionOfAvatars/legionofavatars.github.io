54657ad84afca45cd2a4576687ccd663728ea23d
LegionOfAvatars <49368919+LegionOfAvatars@users.noreply.github.com>
2019-07-04 13:06:42 -0400

    Update membersite_config.php

--- membersite_config.php
+++ membersite_config.php
@@ -4,18 +4,18 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('user11.com');
+$fgmembersite->SetWebsiteName('legionofavatars.github.io');
 
 //Provide the email address where you want to get notifications
-$fgmembersite->SetAdminEmail('user11@user11.com');
+$fgmembersite->SetAdminEmail('legionofavatars@gmail.com');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
 $fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'prasanth',
-                      /*password*/'p',
+                      /*username*/'Legion3Admin',
+                      /*password*/'46ab3vs41234%Ad4@vs!HFSd45f',
                       /*database name*/'testdb',
                       /*table name*/'fgusers3');
 
